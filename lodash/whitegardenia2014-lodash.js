b33bfa8eb547a7bb38826885870c657c6470f77b
gardenia <455419694@qq.com>
2021-07-12 12:16:33 +0800

    lodash-chunk

--- lodash/whitegardenia2014-lodash.js
+++ lodash/whitegardenia2014-lodash.js
@@ -1,8 +1,18 @@
 whitegardenia2014 = function () {
 
 
-  function chunk() {
-
+  function chunk(array, size = 1) {
+    if (array.length == 0) {
+      return []
+    }
+    let length = array.length
+    let res = []
+    let index = 0
+    while (index < length) {
+      res.push(array.slice(index, index + size))
+      index += size
+    }
+    return res
   }
 
 
