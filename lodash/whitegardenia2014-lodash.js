a1645d91c86054c7d1fc2b93fabec9f2da587072
gardenia <455419694@qq.com>
2021-07-27 22:55:13 +0800

    lodash

--- lodash/whitegardenia2014-lodash.js
+++ lodash/whitegardenia2014-lodash.js
@@ -12,6 +12,11 @@ var whitegardenia2014 = function () {
     return typeof value === "function"
   }
 
+  // 判断 value 是不是 Array
+  function isArray(value) {
+    return Array.isArray(value)
+  }
+
   // 判断 value 是不是 ObjectLike，如果 typeof value 为 "object" 并且 value 不是 null，那么 value 是 ObjectLike
   function isObjectLike(value) {
     return typeof value === "object" && value !== null
@@ -29,18 +34,15 @@ var whitegardenia2014 = function () {
       return iteratee
     }
     if (isString(iteratee)) {
-      iteratee = iteratee.trim()
-      let pathArray = iteratee.split(".")
-      if (pathArray.length == 1) {
-        return function (item) {
-          return item[iteratee]
-        }
-      } else {
-        return function (item) {
-          return pathArray.reduce((prev, cur) => prev[cur], item)
-        }
-      }
-
+      return property(iteratee)
+    }
+    if (isArray(iteratee)) {
+      let path = iteratee[0]
+      let srcValue = iteratee[1]
+      return matchesProperty(path, srcValue)
+    }
+    if (isObjectLike(iteratee)) {
+      return matches(iteratee)
     }
   }
 
@@ -86,31 +88,63 @@ var whitegardenia2014 = function () {
 
   // array 数组去重
   function uniq(array) {
-    let myset = new Set()
-    let res = []
-    for (let value of array) {
-      if (!myset.has(value)) {
-        res.push(value)
-        myset.add(value)
+    return Array.from(new Set(array))
+  }
+  /* function uniq(array) 不使用集合的方法
+  function uniq(array) {
+    var result = []
+    for (let i = 0; i < array.length; i++) {
+      if (!result.includes(ary[i])) {
+        result.push(ary[i])
       }
     }
-    return res
+    return result
   }
+*/
 
-  // 依赖 iteratee 的结果，对 array 数组去重
+
+  // 依赖 iteratee 的结果，对 array 数组去重，iteratee 会调用每个元素，得到每个元素需要比较去重的特征
   function uniqBy(array, iteratee) {
     let myset = new Set()
-    let fun = transType(iteratee)
+    let predicate = transType(iteratee)
     let res = []
     for (let value of array) {
-      let fea = fun(value)
-      if (!myset.has(fea)) {
+      let computed = predicate(value)
+      if (!myset.has(computed)) {
         res.push(value)
-        myset.add(fea)
+        myset.add(computed)
       }
     }
     return res
   }
+  /* function uniqBy(array, iteratee) 不使用集合的方法
+    function uniqBy(array, iteratee) {
+      var result = []
+      var seen = []
+      let predicate = transType(iteratee)
+      for (let i = 0; i < array.length; i++) {
+        let computed = predicate(array[i], i, array)
+        if (!seen.includes(computed)) {
+          result.push(ary[i])
+          seen.push(computed)
+        }
+      }
+      return result
+    }
+    */
+
+
+  // 接受 comparator(aryVal, othVal) 作为比较器，比较器用来判断两个元素，是否是重复的
+  function uniqWith(array, comparator) {
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+      if (!result.some(it => comparator(it, array[i]))) {
+        result.push(array[i])
+      }
+    }
+    return result
+  }
+
 
   // 创建一个新数组，将 array 和剩下的值或数组拼接起来
   function concat(array, ...values) {
@@ -270,6 +304,125 @@ var whitegardenia2014 = function () {
     return false
   }
 
+  // 将路径字符串转换为路径数组
+  function toPath(value) {
+    if (isArray(value)) {
+      return value
+    } else {
+      return value.match(/\w+/g)
+    }
+  }
+
+  // 根据路径返回一个对象中路径对应的值，如果值为 undefined，返回 defaultValue 作为替代
+  function get(object, path, defaultValue = undefined) {
+    path = toPath(path)
+
+    for (let i = 0; i < path.length; i++) {
+      if (object == undefined) {
+        return defaultValue
+      } else {
+        object = object[path[i]]
+      }
+    }
+    return object
+
+    // path.reduce((obj, curPath) => {
+    //   return obj[curPath]
+    // }, object)
+  }
+  /* function get() 递归写法
+ function get(object, path, defaultValue = undefined) {
+    if (object == undefined) {
+      return defaultValue
+    } else if (path.length == 0) {
+      return object
+    } else {
+      return get(object[path[0]], path.slice(1))
+    }
+  }
+  */
+
+
+  // 返回 func 的绑定函数，bind 可以接受占位符，实现跳跃的参数绑定，占位符由 bind.placeholder 决定
+  function bind(func, thisArg, ...fixedArgs) {
+    return function (...args) {
+      let ary = fixedArgs.slice()
+      let j = 0
+      // 根据 fixedArgs 和 args 生成新的参数列表，再传递给 func
+      for (let i = 0; i < ary.length; i++) {
+        if (Object.is(ary[i], bind.placeholder)) {
+          if (j < args.length) {
+            ary[i] = args[j++]
+          } else {
+            ary[i] = undefined
+          }
+        }
+      }
+      while (j < args.length) {
+        ary.push(args[j++])
+      }
+      return func.apply(thisArg, ary)
+    }
+  }
+  bind.placeholder = "_"
+
+
+  // 对 object 和 source 进行部分深度比较，判断 object 是否包含与 source 等价的值
+  function isMatch(object, source) {
+    if (object === source) {
+      return true
+    }
+    if (typeof object !== "object" || typeof source !== "object") {
+      return false
+    }
+    for (let key in source) {
+      if (key in object) {
+        if (source[key] && typeof source[key] !== "object") { // 原始值直接进行比较
+          if (object[key] !== source[key]) {
+            return false
+          }
+        } else {
+          if (!isMatch(object[key], source[key])) { // 对象值进行深度比较
+            return false
+          }
+        }
+      } else {
+        return false
+      }
+    }
+    return true
+  }
+
+  // 返回一个函数，该函数接受一个对象参数，判断该对象是否和 src 匹配（即包含 src）
+  function matches(src) {
+    return bind(isMatch, null, "_", src)
+  }
+
+  // 返回一个函数，该函数接受一个对象参数，判断该对象的 path 路径对应的值，是否深度等于 srcValue
+  function matchesProperty(path, srcValue) {
+    return function (obj) {
+      return isEqual(get(obj, path), srcValue)
+    }
+  }
+
+  // 返回一个函数，该函数接受一个对象参数，返回对象的 path 路径对应的值
+  function property(path) {
+    return bind(get, null, "_", path)
+  }
+
+  // 计算数组中值的和，接受 iteratee 参数，对每个元素执行 iteratee，得到的结果作为需要累加的值
+  function sumBy(ary, iteratee = it => it) {
+    let predicate = transType(iteratee)
+    return ary.reduce((sum, item) => {
+      return sum += predicate(item)
+    }, 0)
+  }
+
+  // 计算数组中值的和
+  function sum(ary) {
+    return sumBy(ary)
+  }
+
   // 判断 val 是不是 NaN
   function isNaN(val) {
     if (isObject(val)) {
@@ -305,13 +458,23 @@ var whitegardenia2014 = function () {
     fill: fill,
     uniq: uniq,
     uniqBy: uniqBy,
+    uniqWith: uniqWith,
     groupBy: groupBy,
     keyBy: keyBy,
+    sum: sum,
+    sumBy: sumBy,
     forEach: forEach,
     map: map,
+    get: get,
+    property: property,
+    toPath: toPath,
+    isMatch: isMatch,
+    matches: matches,
+    matchesProperty: matchesProperty,
     isObject: isObject,
     isObjectLike: isObjectLike,
     isFunction: isFunction,
+    isArray: isArray,
     isString: isString,
     isEqual: isEqual,
     isNaN: isNaN,
@@ -320,5 +483,6 @@ var whitegardenia2014 = function () {
     isNil: isNil,
     keys: keys,
     values: values,
+    bind: bind,
   }
 }()
